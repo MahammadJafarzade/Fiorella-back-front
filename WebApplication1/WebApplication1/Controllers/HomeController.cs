4fb7a21ea807341c15687fefbbf74136a8e0b403
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-06-08 09:16:05 +0400

    FiorellaBF

---
+++ WebApplication1/WebApplication1/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.DAL;
+using WebApplication1.Models;
+using WebApplication1.ViewModels;
+
+namespace WebApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private AppDbContext _context { get; }
+        public HomeController(AppDbContext context)
+        {
+            _context=context;
+        }
+        public IActionResult Index()
+        {
+            HomeViewModel home = new HomeViewModel
+            {
+                Slides = _context.Slides.ToList(),
+                SliderSummary =_context.SliderSummary.FirstOrDefault(),
+                Categories = _context.Categories.Where(c=>!c.IsDeleted).ToList(),
+                Products = _context.Products.Where(c => !c.IsDeleted).Include(p=>p.Images).Include(p=>p.Category).ToList()
+            };
+            return View(home);
+        }
+    }
+}
