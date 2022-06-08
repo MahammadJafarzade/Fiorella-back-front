cb731b605c34c5fbefad57fbb496df385160a038
MahammedJafarzade <7a4m9bb@code.edu.az>
2022-06-08 22:44:23 +0400

    FiorellaBaFront

--- WebApplication1/WebApplication1/Areas/AdminPanel/Controllers/CategoryController.cs
+++ WebApplication1/WebApplication1/Areas/AdminPanel/Controllers/CategoryController.cs
@@ -3,15 +3,26 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebApplication1.DAL;
 
 namespace WebApplication1.Areas.AdminPanel.Controllers
 {
     [Area("AdminPanel")]
     public class CategoryController : Controller
     {
+        private AppDbContext _context { get; }
+        public CategoryController(AppDbContext context)
+        {
+            _context = context;
+        }
         public IActionResult Index()
+        {
+            return View(_context.Categories.Where(ct=>!ct.IsDeleted));
+        }
+        public IActionResult Create()
         {
             return View();
         }
     }
+
 }
